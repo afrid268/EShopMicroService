7026d9d5ee7dd3d6ac9b76dc01121060566e4b5c
afrid268 <afridafsal963@gmail.com>
2025-11-17 22:11:24 +0530

    catalog service solution added , product model created

---
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalog.API.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = default!;
+        public List<string> Category { get; set; }  = new();
+        public string Description { get; set; } = default!;
+        public decimal ImageFile { get; set; } = default!;
+    }
+}
