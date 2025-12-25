bbc02d336e6a675ab60727ac6e1a8738a4d4abd1
afrid268 <afridafsal963@gmail.com>
2025-12-25 13:40:59 +0530

    crud minimal api of product finished with docker postgressetup

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Catalog.API.Products.DeleteProduct
+{
+    public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductCommandResponse>;
+    public record DeleteProductCommandResponse(bool IsSuccess);
+    internal class DeleteProductCommandHandler (IDocumentSession session, ILogger<DeleteProductCommandHandler> logger) : ICommandHandler<DeleteProductCommand, DeleteProductCommandResponse>
+    {
+        public async Task<DeleteProductCommandResponse> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("DeleteProductHandler.Handle called with {@Command},command");
+
+            var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+
+            if (product == null)
+            {
+                throw new ProductNotFoundException();
+            }
+
+            session.Delete<Product>(command.Id);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new DeleteProductCommandResponse(true);
+        }
+    }
+}
